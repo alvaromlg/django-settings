5efeaba340d5aa167f9b1c38ebdcd9b4bb4c91fb
Alvaro <alvaro@devel.(none)>
2012-11-05 14:59:03 +0200

    init.py fixed with try catch

--- django_settings/__init__.py
+++ django_settings/__init__.py
@@ -4,8 +4,13 @@ VERSION = (1, 0, 0, 'beta')
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = 'Kuba Janoszek'
 
-from django_settings.models import Setting
-    
+try:
+    from django_settings.models import Setting
+except:
+    pass
+
 def get_setting(name, default=''):
-    return Setting.objects.get_value(setting,default=default)
+    return Setting.objects.get_value(name,default=default)
         
+def set_setting(name, value):
+    Setting.objects.set_value(name, models.Integer, value)
